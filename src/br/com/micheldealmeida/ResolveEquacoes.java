e8c52be6509236df0fa2a9f4b33bb860a9dc3fb2
Michel de Almeida Silva <tensoja@gmail.com>
2013-02-13 11:25:10 -0200

    Adicionado método gaussJordan.

--- src/br/com/micheldealmeida/ResolveEquacoes.java
+++ src/br/com/micheldealmeida/ResolveEquacoes.java
@@ -6,7 +6,6 @@
  */
 package br.com.micheldealmeida;
 
-
 public class ResolveEquacoes {
 
 	private static void pivoParcial(double a[][], double b[]) {
@@ -158,6 +157,63 @@ public class ResolveEquacoes {
 		}
 		return g;
 	}
-	
+
+	public static void gaussJordan(double a[][], double b[]) {
+		int i, j, k, m;
+		double temp;
+
+		int qtdLinhas = a.length;
+		int qtdColunas = a[0].length;
+		int indice[][] = new int[qtdLinhas][2];
+
+		// Determinar o elemento pivô para a coluna atual e reorganizar as
+		// linhas.
+
+		pivoParcial(a, b);
+
+		// Executa a eliminação linha por linha. Primeiro dividindo a linha
+		// atual e o elemento b por a[i][i].
+
+		for (i = 0; i < qtdLinhas; ++i) {
+			temp = a[i][i];
+			for (j = 0; j < qtdColunas; ++j) {
+				a[i][j] /= temp;
+			}
+			b[i] /= temp;
+			a[i][i] = 1.0 / temp;
+
+			// Reduz outras lihas pela subtração de um multiplo da linha atual.
+			// Não reduz a linha atual. Como cada coluna da matriz a[][] é
+			// reduzida os elementos são substituidos pela matriz inversa de
+			// a[][].
+
+			for (k = 0; k < qtdLinhas; ++k) {
+				if (k != i) {
+					temp = a[k][i];
+					for (j = 0; j < qtdColunas; ++j) {
+						a[k][j] -= temp * a[i][j];
+					}
+					b[k] -= temp * b[i];
+					a[k][i] = -temp * a[i][i];
+				}
+			}
+		}
+
+		
+
+		for (j = qtdColunas - 1; j >= 0; --j) {
+			k = indice[j][0];
+			m = indice[j][1];
+			if (k != m) {
+				for (i = 0; i < qtdLinhas; ++i) {
+					temp = a[i][m];
+					a[i][m] = a[i][k];
+					a[i][k] = temp;
+				}
+			}
+		}
+
+		return;
+	}
 
 }
