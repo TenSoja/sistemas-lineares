621165bb51a074aa0235f3f0a6dd5bf5a70f8e4e
Michel de Almeida Silva <tensoja@gmail.com>
2013-02-14 11:56:30 -0200

    Criada classe para o método iterativo GaussSeidel.
    Incluido construtor que recebe a matriz.

---
+++ src/br/com/micheldealmeida/metodositerativos/GaussSeidel.java
@@ -0,0 +1,15 @@
+package br.com.micheldealmeida.metodositerativos;
+
+
+public class GaussSeidel {
+
+	public static final int QTD_MAX_ITERACOES = 100;
+	private double[][] A;
+
+	public GaussSeidel(double[][] a) {
+		A = a;
+	}
+
+	
+
+}
