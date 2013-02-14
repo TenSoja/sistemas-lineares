45bdbc778c49e4eebf649f60c5821fbaffee001f
Michel de Almeida Silva <tensoja@gmail.com>
2013-02-14 11:48:56 -0200

    Criada classe para o método iterativo GaussJacobi
    Construtor incluido para receber a matriz A

---
+++ src/br/com/micheldealmeida/metodositerativos/GaussJacobi.java
@@ -0,0 +1,14 @@
+package br.com.micheldealmeida.metodositerativos;
+
+
+public class GaussJacobi {
+
+	public static final int QTD_MAX_ITERACOES = 100;
+	private double[][] A;
+
+	public GaussJacobi(double[][] a) {
+		A = a;
+	}
+
+	
+}
