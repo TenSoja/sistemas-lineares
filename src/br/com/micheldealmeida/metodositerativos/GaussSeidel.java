227549d2415a8c9bd64a31d425293cd41834b34b
Michel de Almeida Silva <tensoja@gmail.com>
2013-02-14 11:57:38 -0200

    Método resolve adicionado.

--- src/br/com/micheldealmeida/metodositerativos/GaussSeidel.java
+++ src/br/com/micheldealmeida/metodositerativos/GaussSeidel.java
@@ -1,5 +1,6 @@
 package br.com.micheldealmeida.metodositerativos;
 
+import java.util.Arrays;
 
 public class GaussSeidel {
 
@@ -10,6 +11,45 @@ public class GaussSeidel {
 		A = a;
 	}
 
-	
+	public void resolve() {
+		double erro = 1e-15;
+		int iteracao = 0;
+		int tamanho = A.length;
+
+		double[] anterior = new double[tamanho];
+		double[] aproximacao = new double[tamanho];
+
+		Arrays.fill(aproximacao, 0);
+		System.out.println("Iterações:");
+		while (true) {
+			for (int i = 0; i < tamanho; i++) {
+				double soma = A[i][tamanho - 1]; // b_n
+
+				for (int j = 0; j < tamanho; j++)
+					if (j != i)
+						soma -= A[i][j] * aproximacao[j];
+
+				aproximacao[i] = 1 / A[i][i] * soma;
+			}
+
+			System.out.print("X[" + iteracao + "] = {");
+			for (int i = 0; i < tamanho; i++)
+				System.out.print(aproximacao[i] + " ");
+			System.out.println("}");
+
+			iteracao++;
+			if (iteracao == 1)
+				continue;
+
+			boolean pare = true;
+			for (int i = 0; i < tamanho && pare; i++)
+				if (Math.abs(aproximacao[i] - anterior[i]) > erro)
+					pare = false;
+
+			if (pare || iteracao == QTD_MAX_ITERACOES)
+				break;
+			anterior = (double[]) aproximacao.clone();
+		}
+	}
 
 }
